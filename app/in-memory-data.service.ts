168e451c79f092f251af94b4db24e294d7a7482a
jonnywilhite <jonnygwilhite@gmail.com>
2016-10-17 15:35:06 -0500

    finished tutorial

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -1,44 +1,50 @@
-"use strict";
-exports.HEROES = [
-    {
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    let heroes = [
+      {
         id: 11,
         name: 'Narco'
-    },
-    {
+      },
+      {
         id: 12,
         name: 'One Punch Man'
-    },
-    {
+      },
+      {
         id: 13,
         name: 'Bombasto'
-    },
-    {
+      },
+      {
         id: 14,
         name: 'Celeritas'
-    },
-    {
+      },
+      {
         id: 15,
         name: 'Magneta'
-    },
-    {
+      },
+      {
         id: 16,
         name: 'RubberMan'
-    },
-    {
+      },
+      {
         id: 17,
         name: 'Dynama'
-    },
-    {
+      },
+      {
         id: 18,
         name: 'Dr IQ'
-    },
-    {
+      },
+
+      {
         id: 19,
         name: 'Magma'
-    },
-    {
+      },
+      {
         id: 20,
         name: 'Tornado'
-    }
-];
-//# sourceMappingURL=mock-heroes.js.map
+      }
+    ];
+    return {heroes};
+  }
+}
